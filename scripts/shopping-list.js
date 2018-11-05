c811496066d8c00bde02b4639c0fc6be58b79123
seankrummel <sean.krummel@gmail.com>
2018-11-05 11:19:09 -0600

    updated shoppingList to use the new store methods

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -29,12 +29,10 @@ const shoppingList = (function(){
       </li>`;
   }
   
-  
   function generateShoppingItemsString(shoppingList) {
     const items = shoppingList.map((item) => generateItemElement(item));
     return items.join('');
   }
-  
   
   function render() {
     // Filter item list if store prop is true by item.checked === false
@@ -56,7 +54,6 @@ const shoppingList = (function(){
     $('.js-shopping-list').html(shoppingListItemsString);
   }
   
-  
   function addItemToShoppingList(itemName) {
     try {
       Item.validateName(itemName);
@@ -77,12 +74,6 @@ const shoppingList = (function(){
     });
   }
   
-  function toggleCheckedForListItem(id) {
-    const foundItem = store.items.find(item => item.id === id);
-    foundItem.checked = !foundItem.checked;
-  }
-  
-  
   function getItemIdFromElement(item) {
     return $(item)
       .closest('.js-item-element')
@@ -92,19 +83,9 @@ const shoppingList = (function(){
   function handleItemCheckClicked() {
     $('.js-shopping-list').on('click', '.js-item-toggle', event => {
       const id = getItemIdFromElement(event.currentTarget);
-      toggleCheckedForListItem(id);
+      store.findAndToggleChecked(id);
       render();
     });
-  }
-  
-  function deleteListItem(id) {
-    const index = store.items.findIndex(item => item.id === id);
-    store.items.splice(index, 1);
-  }
-  
-  function editListItemName(id, itemName) {
-    const item = store.items.find(item => item.id === id);
-    item.name = itemName;
   }
   
   function toggleCheckedItemsFilter() {
@@ -115,14 +96,13 @@ const shoppingList = (function(){
     store.searchTerm = val;
   }
   
-  
   function handleDeleteItemClicked() {
     // like in `handleItemCheckClicked`, we use event delegation
     $('.js-shopping-list').on('click', '.js-item-delete', event => {
       // get the index of the item in store.items
       const id = getItemIdFromElement(event.currentTarget);
       // delete the item
-      deleteListItem(id);
+      store.findAndDelete(id);
       // render the updated shopping list
       render();
     });
@@ -133,7 +113,7 @@ const shoppingList = (function(){
       event.preventDefault();
       const id = getItemIdFromElement(event.currentTarget);
       const itemName = $(event.currentTarget).find('.shopping-item').val();
-      editListItemName(id, itemName);
+      store.findAndUpdateName(id, itemName);
       render();
     });
   }
