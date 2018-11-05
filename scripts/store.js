4c831f25a8a8b4f838600942f4e3f10838ffea11
seankrummel <sean.krummel@gmail.com>
2018-11-05 11:11:48 -0600

    updated store to create, update, and delete items

--- scripts/store.js
+++ scripts/store.js
@@ -1,4 +1,4 @@
-/*global cuid */
+/*global cuid, Item */
 'use strict';
 
 // eslint-disable-next-line no-unused-vars
@@ -12,5 +12,38 @@ const store = (function() {
   let hideCheckedItems = false;
   let searchTerm = '';
 
-  return {items, hideCheckedItems, searchTerm};
+  function findById(id) {
+    return this.items.find(item => item.id === id);
+  }
+
+  function addItem(name) {
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  }
+
+  function findAndToggleChecked(id) {
+    const item = this.findById(id);
+    item.checked = !item.checked;
+  }
+
+  function findAndUpdateName(id, newName) {
+    try {
+      Item.validateName(newName);
+      const item = this.findById(id);
+      item.name = newName;
+    } catch(error) {
+      console.log('Cannot update name: ' + error.message);
+    }
+  }
+
+  function findAndDelete(id) {
+    // this.items.filter(item => item.id !== id);
+    this.items.splice(this.items.findIndex(item => item.id === id), 1);
+  }
+
+  return {items, hideCheckedItems, searchTerm, findById, addItem, findAndToggleChecked, findAndUpdateName, findAndDelete};
 }());
