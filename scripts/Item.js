1d45ad31386134773440253133e1f38a1c5bb79e
seankrummel <sean.krummel@gmail.com>
2018-11-05 10:39:31 -0600

    added validateNames and create functions to Item module

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,6 +1,16 @@
+/* global cuid */
 'use strict';
 
 // eslint-disable-next-line no-unused-vars
 const Item = (function() {
-  return {};
+  function validateName(name) {
+    if (!name) {
+      throw Error('Name does not exist');
+    }
+  }
+  function create(name) {
+    return {id: cuid(), name, checked: false};
+  }
+
+  return {validateName, create};
 }());
